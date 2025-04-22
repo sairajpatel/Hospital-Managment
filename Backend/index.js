946bdd892fa96719ac96b7d555829b793ffec0f1
sairaj <sairajpatel98@gmail.com>
2025-04-22 12:35:23 -0400

    basic setup of backend  and frontend

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const mongoose= require('mongoose');
+const dotenv= require('dotenv');
+const app= express();
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get('/', (req, res) => {
+    res.json('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
